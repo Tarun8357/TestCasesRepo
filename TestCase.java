7497129edad9998a38f166e6b4dfb70498a9d07f
Tarun8357 <114694793+Tarun8357@users.noreply.github.com>
2025-08-12 17:09:05 +0530

    Update TestCase.java

--- TestCase.java
+++ TestCase.java
@@ -1,29 +1,25 @@
 @Test
-void testCheckKafka_TopicNamesNull_ShouldReturnFalse() throws Exception {
+void testHealth_UpCondition() throws Exception {
     // Arrange
-    when(akafkaAdminClient.listTopics()).thenReturn(listTopicsResult);
-    CompletableFuture<Set<String>> future = CompletableFuture.completedFuture(null);
-    when(listTopicsResult.names()).thenReturn(future);
+    HealthCheck healthCheck = Mockito.spy(new HealthCheck());
+    healthCheck.usageLog = mock(UsageLog.class);
+
+    // Make sure file is writeable to avoid IOException in this case
+    Field fileField = HealthCheck.class.getDeclaredField("healthCheckFile");
+    fileField.setAccessible(true);
+    fileField.set(healthCheck, "test-health-file.txt");
+
+    doReturn(true).when(healthCheck).checkKafka(any());
+    doReturn(true).when(healthCheck).checkDB();
 
     // Act
-    boolean result = healthCheck.checkKafka(akafkaAdminClient);
+    healthCheck.health();
 
     // Assert
-    assertFalse(result);
-}
-
-@Test
-void testCheckKafka_TopicNamesNotNull_ShouldReturnTrue() throws Exception {
-    // Arrange
-    when(akafkaAdminClient.listTopics()).thenReturn(listTopicsResult);
-    Set<String> topicNames = new HashSet<>();
-    topicNames.add("test-topic");
-    CompletableFuture<Set<String>> future = CompletableFuture.completedFuture(topicNames);
-    when(listTopicsResult.names()).thenReturn(future);
-
-    // Act
-    boolean result = healthCheck.checkKafka(akafkaAdminClient);
-
-    // Assert
-    assertTrue(result);
+    verify(healthCheck).checkKafka(any());
+    verify(healthCheck).checkDB();
+    verify(healthCheck.usageLog).logUsageEvent(
+        eq("doHealthCheck()"),
+        contains(Constants.HEALTH_UP)
+    );
 }
