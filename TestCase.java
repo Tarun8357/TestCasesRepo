df4cf988098832c63dd6f64d0fa8e94f75a90c56
Tarun8357 <114694793+Tarun8357@users.noreply.github.com>
2025-08-13 12:47:30 +0530

    Update TestCase.java

--- TestCase.java
+++ TestCase.java
@@ -1,36 +1,80 @@
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mockStatic;
 
-class UsageLogTest {
+class KafkaConfigTest {
+
+    private void setField(Object target, String name, Object value) throws Exception {
+        Field field = target.getClass().getDeclaredField(name);
+        field.setAccessible(true);
+        field.set(target, value);
+    }
 
     @Test
-    void testGetLogger_PublisherBlank() {
-        try (MockedStatic<LogUtils> logUtilsMock = mockStatic(LogUtils.class)) {
-            logUtilsMock.when(() -> LogUtils.getLogAttribute(LogAttributes.PUBLISHER_ID_ATTRIB))
-                        .thenReturn("");
+    void testKafkaConfig_WithSSLAndSASL() throws Exception {
+        try (MockedStatic<DockerSecretsUtil> mockSecrets = mockStatic(DockerSecretsUtil.class)) {
+            Map<String, String> secrets = new HashMap<>();
+            secrets.put("truststorePasswordSecret", "pass123");
+            secrets.put("jaasUserSecret", "user");
+            secrets.put("jaasPassSecret", "pass");
+            mockSecrets.when(DockerSecretsUtil::load).thenReturn(secrets);
 
-            UsageLog usageLog = new UsageLog();
-            Logger logger = usageLog.getLogger();
+            MyKafkaClass kafka = new MyKafkaClass();
+            setField(kafka, "consumerBootstrapServers", "localhost:9092");
+            setField(kafka, "consumerSecurityProtocol", "SASL_SSL");
+            setField(kafka, "consumerSslTruststoreLocation", "/path/truststore.jks");
+            setField(kafka, "consumerSslTruststoreType", "JKS");
+            setField(kafka, "consumerSslTruststorePasswordSecret", "truststorePasswordSecret");
+            setField(kafka, "consumerSaslMechanism", "PLAIN");
+            setField(kafka, "consumerSaslJaasConfigUsernameSecret", "jaasUserSecret");
+            setField(kafka, "consumerSaslJaasConfigPasswordSecret", "jaasPassSecret");
 
-            assertEquals(LoggerFactory.getLogger("USAGE").getName(), logger.getName());
+            Properties props = kafka.kafkaConfig();
+
+            assertEquals("localhost:9092", props.get("bootstrap.servers"));
+            assertEquals("SASL_SSL", props.get("security.protocol"));
+            assertEquals("/path/truststore.jks", props.get("ssl.truststore.location"));
+            assertEquals("JKS", props.get("ssl.truststore.type"));
+            assertEquals("pass123", props.get("ssl.truststore.password"));
+            assertEquals("PLAIN", props.get("sasl.mechanism"));
         }
     }
 
     @Test
-    void testGetLogger_PublisherNotBlank() {
-        try (MockedStatic<LogUtils> logUtilsMock = mockStatic(LogUtils.class)) {
-            logUtilsMock.when(() -> LogUtils.getLogAttribute(LogAttributes.PUBLISHER_ID_ATTRIB))
-                        .thenReturn("PublisherX");
+    void testKafkaConfig_OnlyBootstrapAndSecurityProtocol() throws Exception {
+        try (MockedStatic<DockerSecretsUtil> mockSecrets = mockStatic(DockerSecretsUtil.class)) {
+            mockSecrets.when(DockerSecretsUtil::load).thenReturn(new HashMap<>());
 
-            UsageLog usageLog = new UsageLog();
-            Logger logger = usageLog.getLogger();
+            MyKafkaClass kafka = new MyKafkaClass();
+            setField(kafka, "consumerBootstrapServers", "localhost:9092");
+            setField(kafka, "consumerSecurityProtocol", "PLAINTEXT");
 
-            assertEquals(LoggerFactory.getLogger("USAGE.PublisherX").getName(), logger.getName());
+            Properties props = kafka.kafkaConfig();
+
+            assertEquals("localhost:9092", props.get("bootstrap.servers"));
+            assertEquals("PLAINTEXT", props.get("security.protocol"));
+        }
+    }
+
+    @Test
+    void testKafkaConfig_NoBootstrap_NoProps() throws Exception {
+        try (MockedStatic<DockerSecretsUtil> mockSecrets = mockStatic(DockerSecretsUtil.class)) {
+            mockSecrets.when(DockerSecretsUtil::load).thenReturn(new HashMap<>());
+
+            MyKafkaClass kafka = new MyKafkaClass();
+            setField(kafka, "consumerBootstrapServers", "");
+
+            Properties props = kafka.kafkaConfig();
+
+            // No bootstrap.servers should be set
+            assertEquals(null, props.get("bootstrap.servers"));
         }
     }
 }
