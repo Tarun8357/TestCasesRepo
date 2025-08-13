576f2ebcc2a1ce93fa813794fe501a826ee44466
Tarun8357 <114694793+Tarun8357@users.noreply.github.com>
2025-08-13 12:39:35 +0530

    Update TestCase.java

--- TestCase.java
+++ TestCase.java
@@ -1,72 +1,36 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.slf4j.Logger;
-
-import java.lang.reflect.Method;
-import java.util.Map;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mockStatic;
 
 class UsageLogTest {
 
     @Test
-    void testLogUsageEvent_InfoEnabled_ReflectionOnly() throws Exception {
-        // Mock logger to return true for info enabled
-        Logger mockLogger = mock(Logger.class);
-        when(mockLogger.isInfoEnabled()).thenReturn(true);
+    void testGetLogger_PublisherBlank() {
+        try (MockedStatic<LogUtils> logUtilsMock = mockStatic(LogUtils.class)) {
+            logUtilsMock.when(() -> LogUtils.getLogAttribute(LogAttributes.PUBLISHER_ID_ATTRIB))
+                        .thenReturn("");
 
-        UsageLog usageLog = new UsageLog();
+            UsageLog usageLog = new UsageLog();
+            Logger logger = usageLog.getLogger();
 
-        // Get reference to buildLogMapFromMDC method
-        Method buildLogMapMethod = UsageLog.class.getDeclaredMethod(
-                "buildLogMapFromMDC", Logger.class, String[].class);
-        buildLogMapMethod.setAccessible(true);
-
-        // Get reference to logEvent method
-        Method logEventMethod = UsageLog.class.getDeclaredMethod(
-                "logEvent", String.class, Logger.class, Map.class);
-        logEventMethod.setAccessible(true);
-
-        // Wrap logEvent to intercept arguments
-        final Object[] capturedArgs = new Object[3];
-        Method finalLogEventMethod = logEventMethod;
-        UsageLog interceptingUsageLog = new UsageLog() {
-            @Override
-            protected void logEvent(String severity, Logger logger, Map<String, String> logMap) {
-                capturedArgs[0] = severity;
-                capturedArgs[1] = logger;
-                capturedArgs[2] = logMap;
-            }
-        };
-
-        // Invoke logUsageEvent
-        interceptingUsageLog.logUsageEvent(mockLogger, "testMethod", "testMessage");
-
-        // Assertions on captured arguments
-        assertEquals(LogConstants.INFO_SEVERITY, capturedArgs[0]);
-        assertEquals(mockLogger, capturedArgs[1]);
-        Map<String, String> logMap = (Map<String, String>) capturedArgs[2];
-        assertEquals("testMethod", logMap.get(LogAttributes.METHOD_ATTRIB));
-        assertEquals("testMessage", logMap.get(LogAttributes.MESSAGE_ATTRIB));
+            assertEquals(LoggerFactory.getLogger("USAGE").getName(), logger.getName());
+        }
     }
 
     @Test
-    void testLogUsageEvent_InfoDisabled_ReflectionOnly() throws Exception {
-        Logger mockLogger = mock(Logger.class);
-        when(mockLogger.isInfoEnabled()).thenReturn(false);
+    void testGetLogger_PublisherNotBlank() {
+        try (MockedStatic<LogUtils> logUtilsMock = mockStatic(LogUtils.class)) {
+            logUtilsMock.when(() -> LogUtils.getLogAttribute(LogAttributes.PUBLISHER_ID_ATTRIB))
+                        .thenReturn("PublisherX");
 
-        // Same interception trick
-        final boolean[] called = {false};
-        UsageLog interceptingUsageLog = new UsageLog() {
-            @Override
-            protected void logEvent(String severity, Logger logger, Map<String, String> logMap) {
-                called[0] = true;
-            }
-        };
+            UsageLog usageLog = new UsageLog();
+            Logger logger = usageLog.getLogger();
 
-        interceptingUsageLog.logUsageEvent(mockLogger, "testMethod", "testMessage");
-
-        // Should never call logEvent if info disabled
-        assertEquals(false, called[0]);
+            assertEquals(LoggerFactory.getLogger("USAGE.PublisherX").getName(), logger.getName());
+        }
     }
 }
