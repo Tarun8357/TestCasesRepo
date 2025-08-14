aae173ed6712d48f43ef3990d61224698d0195d7
Tarun8357 <114694793+Tarun8357@users.noreply.github.com>
2025-08-14 13:04:18 +0530

    Update TestCase.java

--- TestCase.java
+++ TestCase.java
@@ -1,17 +1,9 @@
 @Test
-void testRun_countResetBranch() {
+void testRun_runtimeExceptionAfterFetch() {
     KafkaConsumer<String, String> mockConsumer = Mockito.mock(KafkaConsumer.class);
 
-    // Track how many times poll() is called
-    AtomicInteger pollCount = new AtomicInteger(0);
-    Mockito.doNothing().when(mockConsumer).subscribe(Mockito.any());
-    Mockito.when(mockConsumer.assignment()).thenReturn(Collections.emptySet());
-    Mockito.when(mockConsumer.poll(Mockito.any())).thenAnswer(inv -> {
-        if (pollCount.incrementAndGet() >= 101) {
-            throw new WakeupException(); // exit after hitting count >= 100
-        }
-        return new ConsumerRecords<>(Collections.emptyMap());
-    });
+    Mockito.when(mockConsumer.poll(Mockito.any()))
+           .thenThrow(new RuntimeException("Test after fetch"));
 
     KafkaConsumerThread thread = new KafkaConsumerThread(/* args */);
     ReflectionTestUtils.setField(thread, "kafkaConsumer", mockConsumer);
@@ -19,6 +11,3 @@ void testRun_countResetBranch() {
 
     assertDoesNotThrow(thread::run);
 }
-
-
-yiuk
